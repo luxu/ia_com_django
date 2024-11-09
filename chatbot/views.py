9f7df4e7b3ddc3add49eef77bb4b74d67ad42a63
Luciano Martins <zicadopv@gmail.com>
2024-11-08 22:06:51 -0300

    First commit

---
+++ chatbot/views.py
@@ -0,0 +1,78 @@
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import render
+from langchain_groq import ChatGroq
+from markdown import markdown
+
+from chatbot.models import Chat
+from decouple import config
+
+
+def get_chat_history(chats):
+    chat_history = []
+    for chat in chats:
+        chat_history.append(
+            ('human', chat.message)
+        )
+        chat_history.append(
+            ('ai', chat.response)
+        )
+    return chat_history
+
+
+def ask_ai(context, message):
+    try:
+        model = ChatGroq(
+            model='llama-3.2-90b-vision-preview',
+            api_key=settings.GROQ_API_KEY,
+        )
+    except Exception as error:
+        raise error
+    messages = [
+        (
+            'system',
+            'Você é um assistente responsável por tirar dúvidas sobre programação Python.'
+            'Responda em formato markdown.',
+        ),
+    ]
+    messages.extend(context)
+    messages.append(
+        (
+            'human',
+            message,
+        )
+    )
+    print(messages)
+    response = model.invoke(messages)
+    return markdown(response.content, output_format='html')
+
+
+def chatbot(request):
+    template_name = 'chatbot/chatbot.html'
+    chats = Chat.objects.all()
+
+    if request.method == 'POST':
+        context = get_chat_history(
+            chats=chats,
+        )
+        message = request.POST.get('message')
+        response = ask_ai(
+            context=context,
+            message=message
+        )
+
+        chat = Chat(
+            message=message,
+            response=response,
+        )
+        chat.save()
+
+        context = {
+            'message': message,
+            'response': response
+        }
+        return JsonResponse(context)
+    context = {
+        'chats': chats
+    }
+    return render(request, template_name, context)
---
+++ kernel/settings.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+
+from decouple import config, Csv
+from dj_database_url import parse as db_url
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+GROQ_API_KEY = config('GROQ_API_KEY')
+
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+]
+
+THIRD_PARTY_APPS = [
+    'whitenoise',
+]
+
+LOCAL_APPS = [
+    "chatbot",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'kernel.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'kernel.wsgi.application'
+
+default_db_url = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+DATABASES = {
+    "default": db_url(
+        config(
+            'DATABASE_URL',
+            default=default_db_url
+        ),
+        conn_max_age=600,
+        conn_health_checks=True
+    )
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
